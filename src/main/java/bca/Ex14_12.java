1e60ab20a69e0201610abdb3cbdcb173f1b98db2
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2023-09-25 02:11:30 -0400

    Restructured using inheritance

--- src/main/java/bca/Ex14_12.java
+++ src/main/java/bca/Ex14_12.java
@@ -14,23 +14,30 @@ public class Ex14_12 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Pane p = new Pane();
+
+        // Creates a new "resizable prism" object via inheritance
+        ResizablePrism prism = new ResizablePrism();
+        
+        // Basic javafx to create a new scene, set its title, and show the primaryStage
+        Scene scene = new Scene(prism, 425, 425);
+        primaryStage.setTitle("Exercise 14.12");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
+class ResizablePrism extends Pane {
+    public ResizablePrism() {
 
         // Initializes the image view by calling the function
         ImageView view = getRectangularPrism();
 
         // Binds the image view's width and height properties to the width and height properties of its parent, the pane (p)
-        view.fitWidthProperty().bind(p.widthProperty());
-        view.fitHeightProperty().bind(p.heightProperty());
+        view.fitWidthProperty().bind(widthProperty());
+        view.fitHeightProperty().bind(heightProperty());
 
         // Adds the image view to the pane
-        p.getChildren().add(view);
-
-        // Basic javafx to create a new scene, set its title, and show the primaryStage
-        Scene scene = new Scene(p, 425, 425);
-        primaryStage.setTitle("Exercise 14.12");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        getChildren().add(view);
     }
 
     // Simple method that creates an image view and sets it to the image of the rectangular prism for easy access
