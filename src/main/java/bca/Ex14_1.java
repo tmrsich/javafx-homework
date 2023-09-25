c42fd7cca8746c71cc4b42f0323cffd87cec9421
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2023-09-25 03:19:00 -0400

    Restructured for inheritance

--- src/main/java/bca/Ex14_1.java
+++ src/main/java/bca/Ex14_1.java
@@ -8,41 +8,50 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Ex14_1 extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        GridPane gridpane = new GridPane();
-        
-                ImageView[] flagViews = new ImageView[4];
-                String[] flagPaths = {
-                    "file:images/us.gif",
-                    "file:images/uk.gif",
-                    "file:images/ca.gif",
-                    "file:images/china.gif",
-                };
-
-                int row = 0;
-                int col = 0;
-
-                for (int i = 0; i < 4; i++) {
-                    flagViews[i] = new ImageView(new Image(flagPaths[i]));
-                    flagViews[i].setFitWidth(415);
-                    flagViews[i].setFitHeight(250);
-                    gridpane.add(flagViews[i], row, col);
-                    
-                    col++;
-                    if (col > 1) {
-                        col = 0;
-                        row++;
-                    }
-                }
-
-    Scene scene = new Scene(gridpane);
-    primaryStage.setTitle("Exercise 14.1");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-    }
 
     public static void main(String[] args) {
         Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Flags flags = new Flags();
+
+        Scene scene = new Scene(flags);
+        primaryStage.setTitle("Exercise 14.1");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
+class Flags extends GridPane {
+    public Flags() {
+
+        // Initializes an array containing the flag paths that I want to display
+        ImageView[] flagViews = new ImageView[4];
+        String[] flagPaths = {
+            "file:images/us.gif",
+            "file:images/uk.gif",
+            "file:images/ca.gif",
+            "file:images/china.gif"
+        };
+
+        // Initializes rows and columns for later loops
+        int row = 0;
+        int col = 0;
+
+        // For each iteration that i is increased by 1, a new image view is created with flagPaths[i]
+        for (int i = 0; i < 4; i++) {
+            flagViews[i] = new ImageView(new Image(flagPaths[i]));
+            flagViews[i].setFitWidth(415);
+            flagViews[i].setFitHeight(250);
+            add(flagViews[i], row, col);
+
+            col++;
+            if (col > 1) {
+                col = 0;
+                row++;
+            }
+        }
     }
 }
