04f0b5a1ea5b1d45349ad07be68a42d931198f8c
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2023-09-25 02:05:40 -0400

    Binded the imageView's fit width and height properties to the fit width and height properties of its parent element, the pane

--- src/main/java/bca/Ex14_12.java
+++ src/main/java/bca/Ex14_12.java
@@ -2,6 +2,8 @@ package bca;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -14,18 +16,27 @@ public class Ex14_12 extends Application {
     public void start(Stage primaryStage) {
         Pane p = new Pane();
 
+        // Initializes the image view by calling the function
+        ImageView view = getRectangularPrism();
 
+        // Binds the image view's width and height properties to the width and height properties of its parent, the pane (p)
+        view.fitWidthProperty().bind(p.widthProperty());
+        view.fitHeightProperty().bind(p.heightProperty());
 
+        // Adds the image view to the pane
+        p.getChildren().add(view);
 
-
-
-        Scene scene = new Scene(p);
+        // Basic javafx to create a new scene, set its title, and show the primaryStage
+        Scene scene = new Scene(p, 425, 425);
         primaryStage.setTitle("Exercise 14.12");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    
-    
-    
+    // Simple method that creates an image view and sets it to the image of the rectangular prism for easy access
+    public ImageView getRectangularPrism() {
+        ImageView view = new ImageView();
+        view.setImage(new Image("file:images/prism.jpg"));
+        return view;
+    }
 }
