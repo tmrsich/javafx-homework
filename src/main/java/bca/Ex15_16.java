bca20a4e9157f41dd269079c00620c5636c7f0e0
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2023-10-06 15:48:00 -0400

    Finished exercise 15.16

--- src/main/java/bca/Ex15_16.java
+++ src/main/java/bca/Ex15_16.java
@@ -9,6 +9,8 @@ import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+// Authors: Thomas Mrsich, help provided by Mr. Respass, lines 42-52, ellipse and line method for restructuring into the main method
+
 public class Ex15_16 extends Application {
     public static void main(String[] args) {
         Application.launch(args);
@@ -20,17 +22,14 @@ public class Ex15_16 extends Application {
         Pane pane = new Pane();
         Ellipse v1 = getVertice(100, 250, 50);
         Ellipse v2 = getVertice(400, 250, 50);
-        
 
         Line line = new Line();
         line.setStrokeWidth(10);
         line.setStroke(Color.BLACK);
 
         Text text = new Text("Move the circles");
-        text.xProperty().bind((line.startXProperty().add(line.endXProperty())).divide(2));
+        text.xProperty().bind((line.startXProperty().add(line.endXProperty())).divide(2).subtract(30));
         text.yProperty().bind((line.startYProperty().add(line.endYProperty())).divide(2).subtract(10));
-
-
 
         // Binds the line's starting x and y values to the x and y values of the 1st ellipse
         line.startXProperty().bind(v1.centerXProperty());
@@ -41,15 +40,15 @@ public class Ex15_16 extends Application {
         line.endYProperty().bind(v2.centerYProperty());
 
         v1.setOnMouseDragged(event -> {
-            v1.setCenterX(event.getX());
+            v1.setCenterX(event.getX()); 
             v1.setCenterY(event.getY());
-            text.setText("Sample");
+            lineDistance(text, v1, v2);
         });
 
         v2.setOnMouseDragged(event -> {
             v2.setCenterX(event.getX());
             v2.setCenterY(event.getY());
-            text.setText("Sample");
+            lineDistance(text, v1, v2);
         });
 
         // Adds everything to the pane, creates a scene 500x500, sets the title, sets the primaryStage to a specific scene, and shows it
@@ -60,8 +59,17 @@ public class Ex15_16 extends Application {
         primaryStage.show();
     }
 
+    // Private method that returns nothing but sets the text to the calculated distance variable
+    private void lineDistance(Text text, Ellipse v1, Ellipse v2) {
+        double distance = Math.sqrt(Math.pow(v2.getCenterX() - v1.getCenterX(), 2) + Math.pow(v2.getCenterY() - v1.getCenterY(), 2));
+        text.setText("Distance: " + String.format("%.2f", distance) + "Units");
+    }
+
+    // Method to initialize a vertice
     public Ellipse getVertice(int x, int y, int r) {
         Ellipse ellipse = new Ellipse();
+
+        // Sets the various different properties of the vertice
         ellipse.setCenterX(x);
         ellipse.setCenterY(y);
         ellipse.setRadiusX(r);
@@ -69,10 +77,8 @@ public class Ex15_16 extends Application {
         ellipse.setStrokeWidth(1);
         ellipse.setStroke(Color.BLACK);
         ellipse.setFill(Color.RED);
-        ellipse.setOnMousePressed(event -> {
-            ellipse.setCenterX(event.getSceneX());
-            ellipse.setCenterY(event.getSceneY());
-        });
+
+        // Mouse event to trigger whenever the vertices are dragged
         ellipse.setOnMouseDragged(event -> {
             ellipse.setCenterX(event.getSceneX());
             ellipse.setCenterY(event.getSceneY());
