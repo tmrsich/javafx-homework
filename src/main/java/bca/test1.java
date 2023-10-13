2d9f90ffe52ec3de0999631eeec6674556c8a502
Thomas Mrsich <114108783+tmrsich@users.noreply.github.com>
2023-10-13 01:11:04 -0400

    setMargins are now accessed statically, condensed setOnMouseClicked events, and added another card "Ace" that adds 1 to the total "sum"

--- src/main/java/bca/test1.java
+++ src/main/java/bca/test1.java
@@ -28,11 +28,12 @@ public class test1 extends Application {
         FlowPane cards = new FlowPane();
         FlowPane buttons = new FlowPane();
         FlowPane sumLabel = new FlowPane();
-            cards.setMargin(doubleBtn, new Insets(0, 10, 0, 0));
+            FlowPane.setMargin(doubleBtn, new Insets(0, 10, 0, 0));
         VBox vbox = new VBox();
 
         // Creates an array with the card paths
         String[] cardPaths = {
+            "file:images/card/1.png",
             "file:images/card/2.png",
             "file:images/card/3.png",
             "file:images/card/4.png",
@@ -41,11 +42,13 @@ public class test1 extends Application {
         };
 
         // Creates imageviews and sets them to the appropriate card path; Could have also set it to the file names directly, but makes it harder to change in the future
+
         ImageView view1 = new ImageView(new Image(cardPaths[0]));
         ImageView view2 = new ImageView(new Image(cardPaths[1]));
         ImageView view3 = new ImageView(new Image(cardPaths[2]));
         ImageView view4 = new ImageView(new Image(cardPaths[3]));
         ImageView view5 = new ImageView(new Image(cardPaths[4]));
+        ImageView view6 = new ImageView(new Image(cardPaths[5]));
 
         // Mouse events to trigger when the buttons are clicked
         doubleBtn.setOnMouseClicked(event -> {
@@ -58,33 +61,19 @@ public class test1 extends Application {
         });
         
         // Mouse events to trigger when the cards are clicked
-        view1.setOnMouseClicked(event -> {
-            sum += 2;
-            label.setText("Sum: " + sum);
-        });
-        view2.setOnMouseClicked(event -> {
-            sum += 3;
-            label.setText("Sum: " + sum);
-        });
-        view3.setOnMouseClicked(event -> {
-            sum += 4;
-            label.setText("Sum: " + sum);
-        });
-        view4.setOnMouseClicked(event -> {
-            sum += 5;
-            label.setText("Sum: " + sum);
-        });
-        view5.setOnMouseClicked(event -> {
-            sum += 6;
-            label.setText("Sum: " + sum);
-        });
+        view1.setOnMouseClicked(event -> { sum ++; label.setText("Sum: " + sum); });
+        view2.setOnMouseClicked(event -> { sum += 2; label.setText("Sum: " + sum); });
+        view3.setOnMouseClicked(event -> { sum += 3; label.setText("Sum: " + sum); });
+        view4.setOnMouseClicked(event -> { sum += 4; label.setText("Sum: " + sum); });
+        view5.setOnMouseClicked(event -> { sum += 5; label.setText("Sum: " + sum); });
+        view6.setOnMouseClicked(event -> { sum += 6; label.setText("Sum: " + sum); });
 
         // Adds everything to their appropriate flowpane, and adds those flowpanes to 1 vbox, with the cards on the first line, buttons on the 2nd, and the label on the 3rd
         sumLabel.getChildren().addAll(label);
         buttons.getChildren().addAll(doubleBtn, clearBtn);
-        cards.getChildren().addAll(view1, view2, view3, view4, view5);
+        cards.getChildren().addAll(view1, view2, view3, view4, view5, view6);
         vbox.getChildren().addAll(cards, buttons, sumLabel);
-        Scene scene = new Scene(vbox, 400, 300);
+        Scene scene = new Scene(vbox, 450, 300);
         primaryStage.setTitle("Test Question 1");
         primaryStage.setScene(scene);
         primaryStage.show();
